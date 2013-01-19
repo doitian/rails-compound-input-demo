e5830634032d5f8585e4c0245aaefc1216aab329
Ian Yang <me@iany.me>
2013-01-19 20:56:18 +0800

    fields_for scaffold

---
+++ fields_for/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FieldsFor::Application.config.secret_token = 'e967ff9b713a8f24040194e6ed9d3cf75c703b91b92e9c5ce5d13f130247bfae53eb99717bd51cc2ddd3397cc598dc144414c89c3a3ae22a66ca9aa99523505f'
